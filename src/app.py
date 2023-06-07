2f78a7f35e2ed6dca2ff893409c508e668c257a2
penscola <penscolashackletonfelix@gmail.com>
2023-06-07 10:14:31 +0300

    process input data

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Sales Register")
 st.write("Welcome to the Sales Register!")
@@ -10,4 +11,11 @@ petal_height = st.number_input("Enter petal height")
 petal_width = st.number_input("Enter petal width")
 
 if st.button("Predict"):
+
+    df = pd.DataFrame({
+        "sepal_height": [sepal_height], "sepal_width": [sepal_width], "petal_height": [petal_height], "petal_width": [petal_width]
+    })
+
+    print(f"[Info] Input data as dataframe: \n{df.to_markdown()}")
+
     st.text(f"The iris has been classified as: '{''}'")
