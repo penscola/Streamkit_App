4d825bff3844615937bb6cf3d172bf14f462be3e
penscola <penscolashackletonfelix@gmail.com>
2023-06-07 09:22:18 +0300

    Basic streamlit interface

--- src/app.py
+++ src/app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.title("Sales Register")
+st.write("Welcome to the Sales Register!")
+
+sepal_height = st.number_input("Enter sepal heigth")
+sepal_width = st.number_input("Enter sepal width")
+
+petal_height = st.number_input("Enter petal height")
+petal_width = st.number_input("Enter petal width")
+
+if st.button("Predict"):
+    st.text(f"The iris has been classified as: '{''}'")
